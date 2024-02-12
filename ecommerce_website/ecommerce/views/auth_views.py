fd897c46a5ab076d11494685d1c5e4107674c3db
Erik Everton Rodrigues <erik.everton444@gmail.com>
2024-02-12 17:45:50 -0300

    added sign-up logic

--- ecommerce_website/ecommerce/views/auth_views.py
+++ ecommerce_website/ecommerce/views/auth_views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
 from django.views import View
+from django.urls import reverse
+from django.http import HttpResponseRedirect
+from django.contrib.auth.models import User
 from ..forms import SignUpForm
 
 
@@ -7,7 +10,30 @@ class SignUpView(View):
     def get(self, request):
         data = {'form': SignUpForm()}
         return render(request, 'ecommerce/signup.html', data)
+    
+    def post(self, request):
+        form = SignUpForm(data=request.POST)
 
+        if form.is_valid():
+            username = form.cleaned_data.get('username')
+            password1 = form.cleaned_data.get('password1')
+            password2 = form.cleaned_data.get('password2')
+
+            if username and password1 and password2 and password1 == password2:
+                user = User.objects.create_user(
+                    username=username,
+                    password=password1
+                )
+
+                if user:
+                    return HttpResponseRedirect(reverse('login'))
+
+        data = {
+            'form': form,
+            'error': 'Username or password is invalid'
+        }
+
+        return render(request, "ecommerce/signup.html", data)
 
 class LoginView(View):
     def get(self, request):
