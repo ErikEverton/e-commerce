97d8808408cc9d0a128acb9c475fb69f52870b6d
ErikEverton <erik.everton444@gmail.com>
2024-03-16 19:52:01 -0300

    add styles in header and forms

---
+++ ecommerce_website/ecommerce/static/ecommerce/script.js
@@ -0,0 +1,13 @@
+const hamburguer = document.querySelector(".hamburguer")
+const menu = document.querySelector(".menu")
+
+hamburguer.addEventListener("click", () => {
+    if (hamburguer.classList.contains("open")) {
+        hamburguer.classList.remove("open");
+        menu.classList.add("invisible");
+    } else {
+        hamburguer.classList.add("open")
+        menu.classList.remove("invisible");
+    }
+});
+
--- ecommerce_website/ecommerce/urls.py
+++ ecommerce_website/ecommerce/urls.py
@@ -3,6 +3,6 @@ import ecommerce.views as views
 
 app_name = "ecommerce"
 urlpatterns = [
-    path('home/', views.IndexView.as_view(), name="home"),
+    path('', views.IndexView.as_view(), name="home"),
 ]
 
